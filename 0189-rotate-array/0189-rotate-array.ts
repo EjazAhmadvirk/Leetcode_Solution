a21a1b43d4e211ea0793ced789e55fef6a917666
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-10-20 09:59:23 +0500

    Time: 4 ms (50.41%), Space: 66.3 MB (55.83%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -1,17 +1,17 @@
 /**
- Do not return anything, modify nums in-place instead.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-function rotate(nums: number[], k: number): void {
-    const n: number = nums.length;
+var rotate = function (nums, k) {
+    const n = nums.length;
     k %= n;
-    const reverse = (i: number, j: number): void => {
+    const reverse = (i, j) => {
         for (; i < j; ++i, --j) {
-            const t: number = nums[i];
-            nums[i] = nums[j];
-            nums[j] = t;
+            [nums[i], nums[j]] = [nums[j], nums[i]];
         }
     };
     reverse(0, n - 1);
     reverse(0, k - 1);
     reverse(k, n - 1);
-}
+};
