acc2e6f8222877d4454e3503ffac52ec35572895
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-11-10 14:23:21 +0500

    Time: 0 ms (100%), Space: 17.9 MB (30.48%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,23 +1,12 @@
+
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-
-        length = len(s)
-        dp = [0] * (length + 1) # dp[i] => at index i, its decode ways
-
-        dp[length] = 1 # initiator, just make forloop flow working
-        dp[length - 1] = 0 if s[length - 1] == '0' else 1 # assumption is starting at i, so starting as 0 is not decodable
-
-        # start at 'len-2'
-        # or else below 'dp[i+2]' will out of index boundary
-        for i in range(length - 2, -1, -1):
-            if s[i] == '0':
-                continue
-            tem = int(s[i:i + 2])
-
-            if tem > 26:
-                dp[i] = dp[i + 1]
-            else:
-                dp[i] = dp[i + 1] + dp[i + 2]
-        return dp[0]
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for i in range(1, n + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            if i > 1 and s[i - 2] != '0' and (int(s[i - 2]) * 10 + int(s[i - 1]) <= 26):
+                dp[i] += dp[i - 2]
+        return dp[n]
