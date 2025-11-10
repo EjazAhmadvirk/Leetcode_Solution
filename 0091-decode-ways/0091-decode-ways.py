99b3decfbae00889838d047c046ddab85d36f0ee
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-11-10 14:22:30 +0500

    Time: 0 ms (100%), Space: 17.7 MB (89.22%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,16 +1,23 @@
-'''
->>> s = "abcdef"
->>> [(i,c) for i, c in enumerate(s, 1)]
-[(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e'), (6, 'f')]
-'''
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # f: i-2
-        # g: i-1
-        f, g = 0, 1
-        for i, c in enumerate(s, 1):
-            h = g if c != "0" else 0
-            if i > 1 and s[i - 2] != "0" and int(s[i - 2 : i]) <= 26:
-                h += f
-            f, g = g, h
-        return g
+        if len(s) == 0:
+            return 0
+
+        length = len(s)
+        dp = [0] * (length + 1) # dp[i] => at index i, its decode ways
+
+        dp[length] = 1 # initiator, just make forloop flow working
+        dp[length - 1] = 0 if s[length - 1] == '0' else 1 # assumption is starting at i, so starting as 0 is not decodable
+
+        # start at 'len-2'
+        # or else below 'dp[i+2]' will out of index boundary
+        for i in range(length - 2, -1, -1):
+            if s[i] == '0':
+                continue
+            tem = int(s[i:i + 2])
+
+            if tem > 26:
+                dp[i] = dp[i + 1]
+            else:
+                dp[i] = dp[i + 1] + dp[i + 2]
+        return dp[0]
