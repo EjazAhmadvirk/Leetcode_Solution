70f1cf97c7dd6d024cd431d21a58cb767eae98e2
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-11-13 15:19:01 +0500

    Time: 0 ms (100%), Space: 17.9 MB (39.31%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,55 +1,60 @@
+'''
+>>> 17 % 3
+2
+>>> divmod(17,3)
+(5, 2)
+'''
+
+'''
+>>> ['This', 'is', 'an']
+['This', 'is', 'an']
+>>> t = ['This', 'is', 'an']
+>>> spaces = ['    ', '    ']
+
+# note: here j starts from 0
+>>> for j, word in enumerate(t[1:]):
+...     print(j)
+...     print(word)
+...
+0
+is
+1
+an
+'''
+
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        left = 0
-        result = []
-        
-        while left < len(words):
-            right = self.findRight(left, words, maxWidth)
-            result.append(self.justify(left, right, words, maxWidth))
-            left = right + 1
-        
-        return result
-    
-    def findRight(self, left, words, maxWidth):
-        right = left
-        word_length = len(words[right])
-        
-        while (right + 1) < len(words) and (word_length + 1 + len(words[right+1])) <= maxWidth:
-            right += 1
-            word_length += 1 + len(words[right])
-        
-        return right
-    
-    def justify(self, left, right, words, maxWidth):
-        if right - left == 0:
-            return self.padResult(words[left], maxWidth)
-        
-        is_last_line = (right == len(words) - 1)
-        num_spaces = right - left
-        total_space = maxWidth - self.wordsLength(left, right, words)
-        
-        space = " " if is_last_line else " " * (total_space // num_spaces)
-        remainder = 0 if is_last_line else total_space % num_spaces
-        
-        result = ""
-        for i in range(left, right):
-            result += words[i]
-            result += space
-            if remainder > 0:
-                result += " "
-                remainder -= 1
-        
-        result += words[right]
-        result += " " * (maxWidth - len(result))
-        
-        return result
-    
-    def wordsLength(self, left, right, words):
-        words_length = 0
-        for i in range(left, right+1):
-            words_length += len(words[i])
-        return words_length
-    
-    def padResult(self, result, maxWidth):
-        return result + " " * (maxWidth - len(result))
+        def partition(n, cnt):
+            base, mod = divmod(n, cnt)
+            res = [f"{' ' * (base + (i < mod))}" for i in range(cnt)]
+            return res
 
+        ans = []
+        i, n = 0, len(words)
+        while i < n: # one line per iteration
+            t = [words[i]]
+            cnt = len(words[i])
+            i += 1 # move to next word
+            while i < n and cnt + 1 + len(words[i]) <= maxWidth: # greedy search for one line
+                cnt += 1 + len(words[i])
+                t.append(words[i])
+                i += 1
+            if i == n or len(t) == 1:
+                # this is the last line or only one (super-long) word in a line
+                left = ' '.join(t)
+                right = ' ' * (maxWidth - len(left))
+                ans.append(f"{left}{right}")
+                continue # so i != n , so only one word, no need to add spaces. 
+                         # e.g. a word with the same length as maxWidth, 
+                         # or, e.g. the 2nd word is super long and cannot fit in current line
+                # if i==n, then will not enter next while
+            words_width = cnt - (len(t) - 1) # pure words total length, with no spaces
+                                             # spaces in-between these words in t[]: len(t) - 1
+            space_width = maxWidth - words_width
+            spaces = partition(space_width, len(t) - 1)
+            sb = [t[0]] # 1st word of this line, no space before it
+            for j, word in enumerate(t[1:]): # last word has no following space
+                sb.append(spaces[j]) # j starts from 0
+                sb.append(word)
+            ans.append(''.join(sb))
+        return ans
