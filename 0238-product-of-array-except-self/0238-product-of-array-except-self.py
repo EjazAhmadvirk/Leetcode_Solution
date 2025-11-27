e4f89a86c4388bb97fa03ea132c0db848bb1a4ab
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-11-27 06:20:52 +0500

    Time: 27 ms (48.3%), Space: 23.3 MB (55.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,21 +1,12 @@
-class Solution: # 2 extra arrays
-    def productExceptSelf(self, nums):
-        if nums is None or len(nums) == 0:
-            return nums
-
-        # Product from index=0 to index=i-1
-        from_left = [1] * len(nums)
-        for i in range(1, len(nums)):
-            from_left[i] = nums[i - 1] * from_left[i - 1]
-
-        # Product from index=n-1 to current position
-        from_right = [1] * len(nums)
-        for i in range(len(nums) - 2, -1, -1):
-            from_right[i] = nums[i + 1] * from_right[i + 1]
-
-        # Calculate result
-        result = [0] * len(nums)
-        for i in range(len(nums)):
-            result[i] = from_left[i] * from_right[i]
-
-        return result
+class Solution: # follow up, no extra space
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+        left = right = 1
+        for i, v in enumerate(nums):
+            ans[i] = left
+            left *= v
+        for i in range(n - 1, -1, -1):
+            ans[i] *= right
+            right *= nums[i]
+        return ans
